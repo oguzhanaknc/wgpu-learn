043673b7cc82157bc2b1d21b23ed96c47a17cede
Oguzhan Akıncı <oguzzhanakinci@gmail.com>
2023-05-24 18:54:21 +0300

    surface creation seperated

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wgpu::util::DeviceExt;
 mod vertex;
 mod render_pipeline;
+mod surface;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -75,50 +76,11 @@ impl State {
             backends: wgpu::Backends::DX12,
             dx12_shader_compiler: Default::default(),
         });
-        // # Safety
-        //
-        // The surface needs to live as long as the window that created it.
-        // State owns the window so this should be safe.
-        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+      
+        let (surface,device,queue,surface_format,surface_caps) = surface::create_surface(&window, &instance).await;
 
-     
-    
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::HighPerformance,
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            })
-            .await
-            .unwrap();
 
-        let (device, queue) = adapter
-            .request_device(
-                &wgpu::DeviceDescriptor {
-                    features: adapter.features(), // features of the gpu that we want to use (ex: texture compression)
-                    limits: if cfg!(target_arch = "wasm32") {
-                        wgpu::Limits::downlevel_webgl2_defaults()
-                    } else {
-                        wgpu::Limits::default()
-                    },
-                    label: None,
-                },
-                None,
-            )
-            .await
-            .unwrap();
 
-            let surface_caps = surface.get_capabilities(&adapter);
-            // Shader code in this tutorial assumes an sRGB surface texture. Using a different
-            // one will result all the colors coming out darker. If you want to support non
-            // sRGB surfaces, you'll need to account for that when drawing to the frame.
-            let surface_format = surface_caps
-                .formats
-                .iter()
-                .copied()
-                .filter(|f| f.describe().srgb)
-                .next()
-                .unwrap_or(surface_caps.formats[0]);
             let config = wgpu::SurfaceConfiguration {
                 usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
                 format: surface_format,
---
+++ src/surface.rs
@@ -0,0 +1,58 @@
+use wgpu::{Instance, Surface, Queue, Device, TextureFormat, SurfaceCapabilities};
+use winit::window::Window;
+
+
+
+pub async fn create_surface(window: &Window,instance: &Instance) -> (Surface,Device,Queue,TextureFormat,SurfaceCapabilities) {
+    let surface = unsafe { instance.create_surface(window) }.unwrap();
+
+
+    let adapter = instance
+    .request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::HighPerformance,
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+    })
+    .await
+    .unwrap();
+
+
+
+    let (device, queue) = adapter
+    .request_device(
+        &wgpu::DeviceDescriptor {
+            features: adapter.features(), // features of the gpu that we want to use (ex: texture compression)
+            limits: if cfg!(target_arch = "wasm32") {
+                wgpu::Limits::downlevel_webgl2_defaults()
+            } else {
+                wgpu::Limits::default()
+            },
+            label: None,
+        },
+        None,
+    )
+    .await
+    .unwrap();
+
+    let surface_caps = surface.get_capabilities(&adapter);
+
+
+    let surface_format = surface_caps
+        .formats
+        .iter()
+        .copied()
+        .filter(|f| f.describe().srgb)
+        .next()
+        .unwrap_or(surface_caps.formats[0]);
+    let config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: surface_format,
+        width: window.inner_size().width,
+        height: window.inner_size().height,
+        present_mode: surface_caps.present_modes[1], // 0 is fifo (first in first out) 1 is mailbox (newest frame) 2 is immediate (tearing) 3 is vsync
+        alpha_mode: surface_caps.alpha_modes[0],
+        view_formats: vec![],
+    };
+    surface.configure(&device, &config);
+    (surface,device,queue,surface_format,surface_caps)
+}
